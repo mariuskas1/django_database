5c14c62c01cac790c5e2b6056f534eb938fc9355
mariuskas1 <marius.kasparek@gmail.com>
2024-12-07 16:18:13 +0100

    added product and order class

--- sales/models.py
+++ sales/models.py
@@ -5,3 +5,18 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    newsletter_abo = models.BooleanField(default=True)
+    email = models.EmailField(max_length=30, blank=True, default="")
+    account = models.FloatField(blank=True, null=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+
