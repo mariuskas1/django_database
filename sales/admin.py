446942ebfaba4448a707e3af35ebc87fbf5f4771
mariuskas1 <marius.kasparek@gmail.com>
2024-12-09 09:21:09 +0100

    list view/detail view

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Customer, Product, Producttype, Bill, Order
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter=['first_name', 'last_name']
+    list_display=['first_name', 'last_name']
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product)
+admin.site.register(Producttype)
+admin.site.register(Bill)
+admin.site.register(Order)
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Customer
+from django.views.generic.detail import DetailView
+from django.utils import timezone
 
 # Create your views here.
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = "sales/list.html"
+
+
+class CustomerListSearchView(CustomerListView):
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        return Customer.objects.filter(first_name__icontains=name)
+    
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = "sales/detail.html"
+
+    def get_object(self):
+        obj = super().get_object()
+        obj.last_accessed = timezone.now()
+        return obj
